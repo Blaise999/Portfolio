e375a15390b5df698defb672220c23e0c82e0100
Blaise Idoko <blaise999@example.com>
2026-04-16 12:20:29 +0100

    fixed frontend

--- src/data/socials.js
+++ src/data/socials.js
@@ -1,5 +1,5 @@
 export const socials = [
-  { name: "Instagram", handle: "@b_techhhh", url: "https://instagram.com/b_techhh", platform: "ig" },
+  { name: "Instagram", handle: "@b_techhhh", url: "https://instagram.com/b_techhhh", platform: "ig" },
   { name: "X", handle: "@blaisetechh", url: "https://x.com/blaisetechh", platform: "x" },
   { name: "LinkedIn", handle: "Blaisetech", url: "https://www.linkedin.com/in/blaise-tech-582b9a331?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app", platform: "linkedin" },
   { name: "Upwork", handle: "Blaisexx", url: "https://www.upwork.com/freelancers/~011837882186f71a1e?companyReference=2044264649572464874&mp_source=share", platform: "upwork" },
